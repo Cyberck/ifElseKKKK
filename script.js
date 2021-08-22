76dc49e5531b80bfdfdcbf22175a3a4d0a31fd02
cyberck <dushaev2017@gmail.com>
2021-08-22 04:32:58 -0700

    if

--- script.js
+++ script.js
@@ -1,55 +1,80 @@
 
 
+var age = +prompt('vvedite svoy vozrast');
 
-let num = +prompt('Napishite vremya');
-
-switch(num) {
-    case 1:
-    alert(13 + ' Chasov')
-    break;
-    case 2:
-    alert(14 + ' Chasov')
-    break;
-    case 3:
-    alert(15 + ' Chasov')
-    break;
-    case 4:
-    alert(16 + ' Chasov')
-    break;
-    case 5:
-    alert(17 + ' Chasov')
-    break;
-    case 6:
-    alert(18 + ' Chasov')
-    break;
-    case 7:
-    alert(19 + ' Chasov')
-    break;
-    case 8:
-    alert(20 + ' Chasov')
-    break;
-    case 9:
-    alert(21 + ' Chasov')
-    break;
-    case 9:
-    alert(21 + ' Chasov')
-    break;
-    case 10:
-    alert(22 + ' Chasov')
-    break;
-    case 10:
-    alert(22 + ' Chasov')
-    break;
-    case 11:
-    alert(23 + ' Chasov')
-    break;
-    case 12:
-    alert(24 + ' Chasov')
-    break;
-    default:
-    alert('takovo vremeni netu')
-    break;
+if(age <= 18) {
+    alert('vi eshe molodi');
 }
+else if(age > 18 && age <= 50) {
+    alert('vam nujno rabotat');
+}
+else if(age > 50 && age <= 59) {
+    alert('vam skoro na pensiyu');
+}
+else if(age > 59 && age <= 150) {
+alert('vi pensiyoner'); 
+}
+else {
+    alert('Pozdravlyayu takogo vozrosta netu ')
+}
+
+
+
+
+
+
+
+
+
+// let num = +prompt('Napishite vremya');
+
+// switch(num) {
+//     case 1:
+//     alert(13 + ' Chasov')
+//     break;
+//     case 2:
+//     alert(14 + ' Chasov')
+//     break;
+//     case 3:
+//     alert(15 + ' Chasov')
+//     break;
+//     case 4:
+//     alert(16 + ' Chasov')
+//     break;
+//     case 5:
+//     alert(17 + ' Chasov')
+//     break;
+//     case 6:
+//     alert(18 + ' Chasov')
+//     break;
+//     case 7:
+//     alert(19 + ' Chasov')
+//     break;
+//     case 8:
+//     alert(20 + ' Chasov')
+//     break;
+//     case 9:
+//     alert(21 + ' Chasov')
+//     break;
+//     case 9:
+//     alert(21 + ' Chasov')
+//     break;
+//     case 10:
+//     alert(22 + ' Chasov')
+//     break;
+//     case 10:
+//     alert(22 + ' Chasov')
+//     break;
+//     case 11:
+//     alert(23 + ' Chasov')
+//     break;
+//     case 12:
+//     alert(24 + ' Chasov')
+//     break;
+//     default:
+//     alert('takovo vremeni netu')
+//     break;
+// }
 
 
 
