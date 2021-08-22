b2d12fc93188d0552fb41dd24a54cb1bc20b7aff
cyberck <dushaev2017@gmail.com>
2021-08-22 03:59:13 -0700

    switch

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
 
 
 
-// if(1 == '1') {
-//     console.log('Privet');
-// }
+let num = +prompt('Napishite vremya');
 
-
-// let a = prompt('Vvedite svoyo imya');
-
-// if(a === 'Privet') {
-//     console.log('Poka');
-// }else {
-//      console.log('vi holodes');
-// }
-
-// JS urok 2 domashka
-
-
-let a = +prompt('Vvedite svoy vozrast');
-if(a <= 18) {
-    alert('vam nujno uchitsya');
-    let a = +prompt('Vvedite svoy vozrast')
-}else if(a <= 50) {
-    alert('vam nujno rabotat');
-    let a = +prompt('Vvedite svoy vozrast')
-}else if(a <= 59) {
-    alert('vam skoro na pensiyu');
-    let a = +prompt('Vvedite svoy vozrast')
-}else if(a <= 150) { 
-    alert('vi pensioner');
-    let a = +prompt('Vvedite svoy vozrast')    
-}else { alert('Chto-to poshlo ne tak'); 
+switch(num) {
+    case 1:
+    alert(13 + ' Chasov')
+    break;
+    case 2:
+    alert(14 + ' Chasov')
+    break;
+    case 3:
+    alert(15 + ' Chasov')
+    break;
+    case 4:
+    alert(16 + ' Chasov')
+    break;
+    case 5:
+    alert(17 + ' Chasov')
+    break;
+    case 6:
+    alert(18 + ' Chasov')
+    break;
+    case 7:
+    alert(19 + ' Chasov')
+    break;
+    case 8:
+    alert(20 + ' Chasov')
+    break;
+    case 9:
+    alert(21 + ' Chasov')
+    break;
+    case 9:
+    alert(21 + ' Chasov')
+    break;
+    case 10:
+    alert(22 + ' Chasov')
+    break;
+    case 10:
+    alert(22 + ' Chasov')
+    break;
+    case 11:
+    alert(23 + ' Chasov')
+    break;
+    case 12:
+    alert(24 + ' Chasov')
+    break;
+    default:
+    alert('takovo vremeni netu')
+    break;
 }
-
-
-
 
 
 
