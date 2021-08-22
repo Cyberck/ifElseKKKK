7cc8298ee6f3e64342426cab3bdcb88c98806922
cyberck <dushaev2017@gmail.com>
2021-08-22 01:05:30 -0700

    domashka JS urok 2

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+// if(1 == '1') {
+//     console.log('Privet');
+// }
+
+
+// let a = prompt('Vvedite svoyo imya');
+
+// if(a === 'Privet') {
+//     console.log('Poka');
+// }else {
+//      console.log('vi holodes');
+// }
+
+
+
+
+let a = +prompt('Vvedite svoy vozrast');
+if(a <= 18) {
+    alert('vam nujno uchitsya');
+}else if(a <= 50) {
+    alert('vam skoro rabotat');
+}else if(a <= 59) {
+    alert('vam skoro na pensiyu');
+}else if(a <= 150) { 
+    alert('vi pensioner');    
+}else {
+    alert('Chto-to poshlo ne tak')}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
