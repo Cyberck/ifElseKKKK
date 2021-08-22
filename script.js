e004e21ec595995cf1b8c370d21efb70edda491e
cyberck <dushaev2017@gmail.com>
2021-08-22 03:37:00 -0700

    js kod if else

--- script.js
+++ script.js
@@ -17,23 +17,23 @@
 // JS urok 2 domashka
 
 
-// let a = +prompt('Vvedite svoy vozrast');
-// if(a <= 18) {
-//     alert('vam nujno uchitsya');
-//     let a = +prompt('Vvedite svoy vozrast')
-// }if(a <= 50) {
-//     alert('vam nujno rabotat');
-//     let a = +prompt('Vvedite svoy vozrast')
-// }if(a <= 59) {
-//     alert('vam skoro na pensiyu');
-//     let a = +prompt('Vvedite svoy vozrast')
-// }if(a <= 150) { 
-//     alert('vi pensioner');
-//     let a = +prompt('Vvedite svoy vozrast')    
-// }if (alert('Chto-to poshlo ne tak'));
+let a = +prompt('Vvedite svoy vozrast');
+if(a <= 18) {
+    alert('vam nujno uchitsya');
+    let a = +prompt('Vvedite svoy vozrast')
+}else if(a <= 50) {
+    alert('vam nujno rabotat');
+    let a = +prompt('Vvedite svoy vozrast')
+}else if(a <= 59) {
+    alert('vam skoro na pensiyu');
+    let a = +prompt('Vvedite svoy vozrast')
+}else if(a <= 150) { 
+    alert('vi pensioner');
+    let a = +prompt('Vvedite svoy vozrast')    
+}else { alert('Chto-to poshlo ne tak'); 
+}
 
-let a = +prompt('Vvedite svoy vozrast') <= 18 || a <= 50 ? 'vam nujno uchitsya' : 'Chto-to poshlo ne tak';
-    alert(a);
+
 
 
 
